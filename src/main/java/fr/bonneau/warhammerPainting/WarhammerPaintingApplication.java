fd5ee88020505db1b9ce93813d36098a7ddd5d46
Pierre <pschouteeten@excilys.com>
2020-11-30 12:10:34 +0100

    fist commit

---
+++ src/main/java/fr/bonneau/warhammerPainting/WarhammerPaintingApplication.java
@@ -0,0 +1,13 @@
+package fr.bonneau.warhammerPainting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarhammerPaintingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarhammerPaintingApplication.class, args);
+	}
+
+}
