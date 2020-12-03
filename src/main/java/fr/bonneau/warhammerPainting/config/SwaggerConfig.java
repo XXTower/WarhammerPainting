d151ac66c28b766927fa648f325692bb464cb990
pierre <xx-tower@hotmail.fr>
2020-12-03 15:24:34 +0100

    Add config for swagger3

---
+++ src/main/java/fr/bonneau/warhammerPainting/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package fr.bonneau.warhammerPainting.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+    /**
+     * Bean utilisé par Swagger pour récupérer sa configuration.
+     *
+     * @return {@link Docket}
+     */
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(("fr.bonneau.warhammerPainting.controller")))
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
